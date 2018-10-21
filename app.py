dcc13dab05f6863b015ea2a32e6f18ce74990f94
Jessica Brock <jessicabrock03@gmail.com>
2018-10-21 10:18:51 -0700

    testing channel listing

---
+++ app.py
@@ -0,0 +1,27 @@
+"""Simple Slack API testing"""
+import os
+from slackclient import SlackClient
+
+# get from environment variable
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN')
+SLACK_CLIENT = SlackClient(SLACK_TOKEN)
+
+
+def list_channels():
+    """ should return two keys: ok and channels
+        ok should return True if API call was successful
+        channels will then return a list of channels
+    """
+    channels_call = SLACK_CLIENT.api_call("channels.list")
+    if channels_call.get('ok'):
+        return channels_call['channels']
+    return None
+
+if __name__ == '__main__':
+    channels = list_channels()
+    if channels:
+        print("Channels: ")
+        for c in channels:
+            print(c['name'] + " (" + c['id'] + ")")
+    else:
+        print("Unable to authenticate.")
