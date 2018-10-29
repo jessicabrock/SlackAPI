de6b5cc68f09289938ba93fb4490e980d37a7cbf
Jessica Brock <jessicabrock03@gmail.com>
2018-10-28 17:15:21 -0700

    adding test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+""" Unit Testing SlackAPI app """
+import unittest
+import app
+
+class TestApp(unittest.TestCase):
+
+    def test_token(self):
+        # STOPPED
+
+if __name__ == '__main__':
+    unittest.main()
