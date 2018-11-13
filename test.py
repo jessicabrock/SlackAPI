a682ba060d2e21086b869a97f6c79ea5b831dec5
Jessica Brock <jessicabrock03@gmail.com>
2018-11-12 17:23:11 -0800

    added tests

--- test.py
+++ test.py
@@ -14,17 +14,23 @@ class TestApp(unittest.TestCase):
         sc = app.SlackClient(app.os.getenv('SLACK_TOKEN'))
         self.assertTrue(sc.api_call("auth.test"), 'ok')
 
-    def test_channels(self):
+    def test_channel_listing(self):
         """ test retrieving channel listing """
         channels_call = app.SLACK_CLIENT.api_call("channels.list")
         get_channels = channels_call.get('ok')
         self.assertTrue(get_channels,'ok')
 
+    def test_get_channel(self):
+        channel_info = app.SLACK_CLIENT.api_call("channels.info")
+        self.assertTrue(channel_info.get('ok'))
+        # self.assertIn(channel_info,'C8EV35W3B') # channel_info is DICT
+
     def main():
         """ entry point """
         test_token
         test_auth
-        test_channels
+        test_channel_listing
+        test_get_channel
 
 if __name__ == '__main__':
     unittest.main()
